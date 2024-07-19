ae12f55f16867300a1c8e61cd2292c1ca3b284f7
Amardeep <121479647+Amardeepk2468@users.noreply.github.com>
2024-07-20 02:10:02 +0530

    changes in  AddNote components

--- frontend/src/components/AddNote.jsx
+++ frontend/src/components/AddNote.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function AddNote() {
   return (
     <Link to={'/add'}>
-        <button className='addNote'>Add Note</button>
+        <button className='addNote'>Add Note Here </button>
     </Link>
   )
 }
